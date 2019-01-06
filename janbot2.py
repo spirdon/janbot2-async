952768f178ece504b0cbbd71cb8d66326bc48e69
spierdon <spierdon@wp.pl>
2019-01-06 23:55:21 +0100

    Added hardcoded event logging

--- janbot2.py
+++ janbot2.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 import logging
+import discord
 
 from jb2.bot import bot
 from jb2.text.elo import elo
@@ -9,14 +10,50 @@ from jb2.text.przondlo import przondlo
 from jb2.text.szkaluj import szkaluj
 from jb2.text.ufnal import ufnal
 
-logging.basicConfig(
-    level='DEBUG', format='%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s: %(message)s')
 logger = logging.getLogger('janbot2')
 
 @bot.event
 async def on_ready():
     logger.info("JanBot2 gotowy!")
     logger.info("Zalogowano jako: {}".format(bot.user.name))
+
+log_channel_id = 531586622026678293
+discordchan_id = 411952528499015691
+
+@bot.event
+async def on_message_delete(message):
+    if message.guild.id != discordchan_id:
+        return
+    if message.channel.id == log_channel_id:
+        return
+
+    channel = bot.get_channel(log_channel_id)
+
+    emb = discord.Embed(title="{} usunięto wiadomość".format(message.author))
+    emb.description = "[{}#{}] {}".format(message.guild.name, 
+                                          message.channel.name, 
+                                          message.content)
+
+    await channel.send(embed=emb)
+
+@bot.event
+async def on_message_edit(before, after):
+    if before.guild.id != discordchan_id:
+        return
+    if before.channel.id == log_channel_id:
+        return
+
+    channel = bot.get_channel(log_channel_id)
+
+    emb = discord.Embed(title="{} zedytował wiadomość".format(before.author))
+    emb.description = "[{}#{}]\nPrzed:\n{}\n\nPo:\n{}".format(
+        before.guild.name,
+        before.channel.name, 
+        before.content,
+        after.content)
+
+    await channel.send(embed=emb)
 
 if __name__ == '__main__':
     TOKEN = os.environ['DISCORD_TOKEN']
