c78565398d383a2097972e31ab3511439398c1e6
spierdon <spierdon@wp.pl>
2019-01-06 19:51:43 +0100

    Modified elo command

--- jb2/text/elo.py
+++ jb2/text/elo.py
@@ -1,5 +1,20 @@
+import random
+import discord
+
 from jb2.bot import bot
+
+with open('res/text/elo.txt') as file:
+    global greetings
+    greetings = file.readlines()
 
 @bot.command()
 async def elo(ctx):
-    await ctx.send("No elo.")
+    global greetings
+    
+    emoji = ':wave:'
+    answer = random.choice(greetings)
+
+    text = '{} {}: {}'.format(emoji, ctx.author.mention, answer)
+    emb = discord.Embed(description=text, color=0xffffaa)
+    
+    await ctx.send(embed=emb)
